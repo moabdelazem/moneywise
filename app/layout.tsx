6229a69a9eab59edc795126ff12cb9e35cd0bd01
Mohamed Abdelazem <mabdelazemahmed@gmail.com>
2025-04-17 20:06:24 +0200

    feat: integrate @vercel/speed-insights for performance monitoring in the application

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "next-themes";
 import { usePathname } from "next/navigation";
 import { AnimatePresence } from "framer-motion";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
+        <SpeedInsights/>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
